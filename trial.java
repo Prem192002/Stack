fc357eb36e40648a7a24f610a144928738c5491e
unknown <premsingh192002@gmail.com>
2024-08-28 23:44:38 +0530

    Initial commit

---
+++ trial.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class trial {
+    public static void main(String[] args) {
+    Stack<Integer> st = new Stack<>();
+    st.push(5);
+    st.push(10);
+    st.push(15);
+    st.push(20);
+    st.push(25);
+
+    }
+}
