28a6631df34badf10b0b35a0161054385ce6fdec
unknown <premsingh192002@gmail.com>
2024-09-12 01:15:31 +0530

    second commit

--- Array_implementation.java
+++ Array_implementation.java
@@ -1,62 +1,56 @@
-public class Array_implementation {
-
-    private static class Stack {
-        int[] arr = new int[5];
-        int idx = 0;
-        
-        public void push(int num) {
-            if (idx < arr.length) {
-                arr[idx] = num;
-                idx++;
-            } else {
-                System.out.println("Stack Overflow");
-            }
-        }
-        
-        public int pop() {
-            if (idx > 0) {
-                int top = arr[idx - 1];
-                arr[idx - 1] = 0; // Optional: Clear the popped value
-                idx--;
-                return top;
-            } else {
-                System.out.println("Stack Underflow");
-                return -1; // Return a sentinel value indicating the stack is empty
-            }
-        }
-        
-        public int peek() {
-            if (idx > 0) {
-                return arr[idx - 1];
-            } else {
-                System.out.println("Stack is empty");
-                return -1; // Return a sentinel value indicating the stack is empty
-            }
-        }
-        
-        public void display() {
-            for (int i = 0; i < idx; i++) {
-                System.out.print(arr[i] + " ");
-            }
-            System.out.println();
-        }
-    }
+public class Array_implementation{
     
-    
-    
-    
-    
-    public static void main(String[] args) {
-        Stack st = new Stack();
-        st.push(5);
+    public static void main(String[] args){
+        stack st = new stack();
+        st.push(2);
+        st.push(4);
+        st.push(6);
+        st.push(8);
         st.push(10);
-        st.push(15);
-        st.push(20);
-        st.push(25);
         st.display();
+
+        System.out.println();
 
         st.pop();
         st.display();
     }
 }
 
+class stack{
+    int[] arr = new int[5];
+    int idx = 0;
+    //[2,4,6,8,10,12,14,16,18,20]
+    public void push(int num){
+        if(idx>=arr.length){
+            System.out.println("stack overflow");
+            return;
+        }
+        else{
+        arr[idx]=num;
+        idx++;
+        }
+    }
+
+    public int pop(){
+        if(idx>0){
+        int temp=arr[idx-1];
+        arr[idx-1]=0;
+        idx--;
+        return temp;
+        }
+        else{
+            System.out.println("stack underflow");
+            return -1;
+        }
+    }
+
+    public int peek(){
+        return arr[idx-1];
+    }
+
+    public void display(){
+        for(int i=0;i<idx;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
